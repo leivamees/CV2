d934a70907db01384c73faf93d29c8863f8dce0e
Janno Kink <jannokink@Jannos-MBP.lan>
2019-08-16 21:25:43 +0300

    client folder

---
+++ client/build/precache-manifest.768bdbe29cc27eb11ed4505f4bdb1f91.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0754d537d9b0298d2c5e8176cfdd015",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a498372f9e4679073dce",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "780afd5c383ca756d6ad",
+    "url": "/static/js/2.bce15827.chunk.js"
+  },
+  {
+    "revision": "a498372f9e4679073dce",
+    "url": "/static/js/main.74ecaea0.chunk.js"
+  },
+  {
+    "revision": "5fe5c3fe5aeb581ff41b",
+    "url": "/static/js/runtime~main.30fc1286.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
